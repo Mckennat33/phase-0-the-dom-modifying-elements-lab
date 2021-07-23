daf86a1b8225f7a7dbdfa156553893752867ff51
thomasmckenna <thomasmckenna12@gmail.com>
2021-07-22 20:08:52 -0400

    /Users/thomasmckenna/phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here!
+
+// const element = document.removeChild('main')
+
+const main = document.querySelector('#main'); 
+
+main.remove(); 
+
+
+//question 
+const newHeader = document.createElement('h1'); 
+
+newHeader.setAttribute("id", "victory"); 
+
+//for some reason this innerHtml has to be before the document.body.append 
+newHeader.innerHTML = "Thomas is the champion"
+
+document.body.append(newHeader); 
+
+// adding a string into that header. 
+// newHeader.innerHTML("Thomas is the champion")
+
+
+
