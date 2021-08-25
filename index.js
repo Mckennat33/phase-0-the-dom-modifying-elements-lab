9d056c957895690731bc847e48fcaf7ba19eacac
Thomas <thomasmckenna12gmail.com>
2021-08-25 11:31:13 -0400

    commit tree e5faa6a00b1a6c493c4a4371e5d843bde16c06a3

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 // Write your code here!
+// 
+ // const element = document.removeChild('main')
+ 
+ // const main = document.querySelector('#main'); 
+ // 
+ // main.remove(); 
+ // 
+ // 
+ // //question 
+ // const newHeader = document.createElement('h1'); 
+ // 
+ // newHeader.setAttribute("id", "victory"); 
+ // 
+ // //for some reason this innerHtml has to be before the document.body.append 
+ // newHeader.innerHTML = "Thomas is the champion"
+ // 
+ // document.body.append(newHeader); 
+ // 
+ // // adding a string into that header. 
+ // newHeader.innerHTML("Thomas is the champion")
 
-// const element = document.removeChild('main')
 
-const main = document.querySelector('#main'); 
+// phase 1 lab over again 
 
-main.remove(); 
+// question 1 
+const main = document.querySelector("#main");
+
+main.remove('id', 'main')
+
+//question 2
 
 
-//question 
-const newHeader = document.createElement('h1'); 
 
-newHeader.setAttribute("id", "victory"); 
+const newHeader = document.createElement("h1");
 
-//for some reason this innerHtml has to be before the document.body.append 
+newHeader.setAttribute("id", "victory")
 newHeader.innerHTML = "Thomas is the champion"
+document.body.append(newHeader)
 
-document.body.append(newHeader); 
-
-// adding a string into that header. 
-// newHeader.innerHTML("Thomas is the champion")
-
-
-
+ // newHeader.innerHTML('Thomas is the Champion')
